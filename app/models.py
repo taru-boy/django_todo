d4f8d63d21fd8c2264df84aea24cb9de858f8504
taru-boy <tarutaru.com.5021@gmail.com>
2025-07-27 10:13:02 +0900

    day15

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField("タイトル", max_length=100)
+    deadline = models.DateField("期限")
+
+    def __str__(self) -> str:
+        return self.title
