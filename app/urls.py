6a092de06015c50e7251d00c396579903863b2d2
taru-boy <tarutaru.com.5021@gmail.com>
2025-08-01 20:47:30 +0900

    day18

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from app import views
 
-urlpatterns = [path("", views.IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/edit/", views.EditView.as_view(), name="edit"),
+]
--- app/views.py
+++ app/views.py
@@ -23,3 +23,29 @@ class IndexView(View):
             todo_data.deadline = form.cleaned_data["deadline"]
             todo_data.save()
         return redirect("index")
+
+
+class EditView(View):
+    def get(self, request, *args, **kwargs):
+        todo_data = Todo.objects.get(id=self.kwargs["pk"])
+        form = TodoForm(
+            request.POST or None,
+            initial={
+                "title": todo_data.title,
+                "deadline": todo_data.deadline,
+            },
+        )
+
+        return render(request, "app/edit.html", {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = TodoForm(request.POST or None)
+
+        if form.is_valid():
+            todo_data = Todo.objects.get(id=self.kwargs["pk"])
+            todo_data.title = form.cleaned_data["title"]
+            todo_data.deadline = form.cleaned_data["deadline"]
+            todo_data.save()
+            return redirect("index")
+
+        return redirect("edit", pk=self.kwargs["pk"])
